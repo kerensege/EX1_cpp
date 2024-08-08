5d8bd10ffd1cd4b1145c691853ff863af3d3d4ef
kerensege <118932424+kerensege@users.noreply.github.com>
2024-08-08 16:39:36 +0300

    Update Algorithms.hpp

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -5,10 +5,11 @@ class Algorithms{
     public:
         static int isConnected(Graph g);
         static int shortestPath(Graph g,int start,int end);
-        static int isContainCycle(Graph g);
+        static int isContainsCycle(Graph g);
         static Graph isBipartite(Graph g);
         static void negativeCycle(Graph g);
-        static void isVisited(Graph g,int v,bool visited[],int previous[]);
+        static void isVisited(Graph g,int v,bool visited[],int previous[], int* end);
+        static int negativeFinder(Graph g, int* dist, int* pai, int start);
     
     private:
         static void DFS(Graph g,int v,bool visited[]);
